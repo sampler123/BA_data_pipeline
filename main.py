963eca1c0ec2d48c8234f5af3e38cbc321d05aa0
NICO-Windows <acc.goetzfried.nicolas@gmail.com>
2021-06-04 21:11:47 +0200

    making things pretty

--- main.py
+++ main.py
@@ -1,45 +1,57 @@
-# This is a sample Python script.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = "Nicolas Götzfried"
+"""
+Data processing script for Choice-Based Conjoint Analysis conducted via QuestionPro.
+The script validate respondents based on three holdouts. Holdout choice set 1 and 3 have to be identical.
+The script transforms the QuestionPro wide format into long format. It changes the following:
+1. Mapping attributes to levels with dummy variables
+2. Adding a none option for every conjoint tasks
+3. calculating a time variable, which is necessary for Cox regression
+4. enables a priori segmentation via "respondent list"
+"""
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import pandas as pd
-import numpy as np
+import pandas as pd  # third-party library
+import numpy as np  # third-party library
 
 
 def validate_holdouts():
-    # return list of valid respondents
-    is_valid = []
+    is_valid = []  # return list of valid respondents
     for respondent in holdout_respondents:
-        # get a dataset for each respondent containing all selected holdout decisions. Containing 0-3 entries, depending on how many times the respondet choose the none option (questionPro doesnt save those)
+        # Iterating through respondentsIDs
+        # The dataset size various from 0 to 3, depending on how many times a respondent selected 'none'
+        # QuestionPro doesnt export the row, if 'none is selected'
+
+        # dataframe containing rows of selected holdout profiles
         profile_respondent = holdouts.loc[
             (holdouts["Response ID"] == respondent) & (holdouts["Selected"] == 1)]
-        # deciding how many times a none option was selected, 1 = 2 times, 2 = 1 time, 3 = 0 times
-        size = len(profile_respondent)
-        # 2 times none option, valid responded must have concept 3 or 4.
-        if size == 1:
+
+        size = len(profile_respondent)  # size indicates how many times 'none option' was selected
+
+        if size == 1:  # size 1 => respondent selected 2 times 'none'
             if (profile_respondent.iloc[0]["Concept ID"] == 3) or (profile_respondent.iloc[0]["Concept ID"]) == 4:
                 is_valid.append(respondent)
         else:
-            # 1 none option, valid response must have a difference between 4 of the selected concepts [0], [1]
-            if size == 2:
+            if size == 2:  # size 2 => respondent selected 1 time 'none': e.g. concept 1  == concept 5
                 if (profile_respondent.iloc[0]["Concept ID"] + 4) == profile_respondent.iloc[1]["Concept ID"]:
                     is_valid.append(respondent)
-            # No None option, valid response must have a difference between 4 of the selected concepts [0], [2]
-            else:
+            else:  # size 3 => no none option: same as size 2
                 if (profile_respondent.iloc[0]["Concept ID"] + 4) == profile_respondent.iloc[2]["Concept ID"]:
                     is_valid.append(respondent)
-    return is_valid
+    return is_valid  # list of valid respondent ids
 
 
 def to_long_format(wide, r_id):
     rows_list = []
     for i in range(0, len(wide.index)):
-        # second rows are the interesting ones, if more concepts are displayed to the user "(i-1) % 2 == 0" has to be updated
+        # QuestionPro does not export a third row for concept 3 none
+        # second rows are the interesting ones, if more concepts are displayed
+        # to the user "(i-1) % 2 == 0" has to be updated
         # Creating a List of Rows -> transforming from attributes to levels + adding none option
         # checking if respondent belongs to the filtered respondent list (which im interested in)
         if wide.iloc[i]["Response ID"] in r_id:
             # is it the second concept? if true get row 1 and row 2 , row 3 is none option
-            if (i-1) % 2 == 0:
+            if (i - 1) % 2 == 0:
                 print("index: " + str(i))
                 row1 = wide.iloc[i - 1]
                 row2 = wide.iloc[i]
@@ -49,8 +61,8 @@ def to_long_format(wide, r_id):
                 row1_long = []
                 row2_long = []
                 row3_long = []
-                # adding the mapping from wide fromat (1-4) to binary long format (0,1)
-                choices = {1: [1, 0, 0, 0], 2: [0, 1, 0, 0], 3: [0, 0, 1, 0], 4: [0, 0, 0, 1]} # hashmap-> wide->long
+                # adding the mapping from wide format (1-4) to binary long format (0,1)
+                choices = {1: [1, 0, 0, 0], 2: [0, 1, 0, 0], 3: [0, 0, 1, 0], 4: [0, 0, 0, 1]}  # hashmap-> wide->long
                 for j in range(len(row2)):
                     print("index j:" + str(j))
                     # Respondent, Task ID
@@ -76,6 +88,7 @@ def to_long_format(wide, r_id):
                         row3_long.append(1)
                     # selected, if case ow1[j] == 0 & row2[j] == 0: none option is selected
                     elif j == 10:
+                        # If concept 1 and concept 2 is not selected, concept 3 'none' is selected
                         if row1[j] == 0 and row2[j] == 0:
                             print(row1[j])
                             print(row2[j])
@@ -83,7 +96,6 @@ def to_long_format(wide, r_id):
                             row2_long.append(0)
                             row3_long.append(1)
                         else:
-                            #[[1621145591, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1]
                             row1_long.append(row1[j])
                             row2_long.append(row2[j])
                             row3_long.append(0)
@@ -95,45 +107,43 @@ def to_long_format(wide, r_id):
 
 
 if __name__ == '__main__':
-    # 01 reading DATA
-    # reading Full Profile for all Respondents (7)
+    # reading "Full Profile" for all Respondents, 7 conjoint tasks each respondent
     full_profile = pd.read_csv("Full_profiles.CSV", delimiter=";")
-    # reading holdouts (3)
+    # reading holdouts, 3 holdout tasks each respondent
     holdouts = pd.read_csv("holdouts.CSV", delimiter=";")
     # reading a list of respondents im interested in (filtering done by spss)
     conditionCSV = "only_female.csv"
     interested_respondents = pd.read_csv(conditionCSV)
     # addtional Information for CBCA
-    #tasks = 7
-    #concepts = 2
+    # tasks = 7
+    # concepts = 2
 
     # level of attributrs
     goals_level = ["weekly goals", "menu planning exercise", "set amount of calorie intake",
-                 "daily habit challenge per week"]
+                   "daily habit challenge per week"]
     tracking_level = ["tracking via food-database", "tracking via recipes", "Import recipes from websites",
-                     "track via photo"]
+                      "track via photo"]
     reinforcement_level = ["life-score", "surprise challenges", "tracking reminder", "food displayed in color"]
     selfefficay_level = ["food diary", "progress graph of intake", "confidence scale", "feedback on goal progress"]
     socialsupport_level = ["interaction with Family Friends Peers", "invite person to track users food",
-                          "role models performing target behavior", "Sharing achievements"]
+                           "role models performing target behavior", "Sharing achievements"]
     provider_level = ["non profit organization", "well known company", "start-up", "insurance company"]
 
     # attributes in English (questionPro export is german(default language)) "Ziele" -> "goals"
     renamed_columns = ['Response ID', 'Task ID', 'Concept ID', 'goals', 'tracking', 'reinforcement',
                        'self-efficay', 'social support', 'provider', 'Parts Worth', 'selected',
                        'Standard Deviation', 'Confidence Interval Range 1', 'Confidence Interval Range 2']
-    # 02 preparing Data
+
     # renaming attributes from German to English
     full_profile.columns = renamed_columns
 
-    # adding a new level "none" at position 10 to the dataset (filled with dummy value -1)
+    # adding a new level "none" at index 10 to the dataset (filled with dummy value -1)
     full_profile["none"] = -1
     columns = list(full_profile.columns)
     full_profile = full_profile[columns[:9] + [columns[-1]] + [columns[10]]]
 
-    # adding "time" to the end of the dataset with dummy values -1 (compare Backhaus et al)
+    # adding "time" to the end of the dataset with dummy values -1
     full_profile["time"] = -1
-
 
     all_respondents = pd.unique(full_profile["Response ID"])
     holdout_respondents = pd.unique(holdouts["Response ID"])
@@ -141,12 +151,11 @@ if __name__ == '__main__':
     # symmetric difference for respondents which selected 3 times the none option (These are valid respondents)
     always_none_respondents = list(set(all_respondents) - set(holdout_respondents))
 
-    # Data Cleaning
-    # getting valid respondents (checking holdouts) (list of valid respondents
+    # getting valid respondents (checking holdouts) (list of valid respondents)
     valid_respondents = validate_holdouts()
-    #valid_respondents = [1621145591, 1621146858]
 
-    # Adding User who picked 3 times none in the holdouts
+    # Adding User who picked 3 times none in the holdouts, they are not part of the holdouts
+    # dataset, because QuestionPro does not export the none option rows
     for resp in always_none_respondents:
         valid_respondents.append(resp)
 
@@ -159,7 +168,7 @@ if __name__ == '__main__':
     df_final = pd.DataFrame(long_format, columns=final_arr)
 
     # adding time variable for Cox Regression (Backhaus et.al) (-(selected-2))
-    df_final["time"] = (-(df_final["selected"]-2))
+    df_final["time"] = (-(df_final["selected"] - 2))
 
     # saving Data
     df_final.to_csv(conditionCSV[:-4] + "_long_clean.csv", sep=";", index=False)
